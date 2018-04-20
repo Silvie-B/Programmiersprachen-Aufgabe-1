452aa696c34854247d084ec81fc09db82b5b3308
Silvie Böning <silvie.boening@t-online>
2018-04-20 14:30:12 +0200

    test.cpp fertig

--- source/tests.cpp
+++ source/tests.cpp
@@ -6,12 +6,14 @@ int gcd(int a, int b)
 {
     return 1;
 }
+
 TEST_CASE("describe_gcd" , "[gcd]")
 {
     REQUIRE(gcd(2,4) == 2);
     REQUIRE(gcd(9,6) == 3);
     REQUIRE(gcd(3,7) == 1);
 }
+
 int main(int argc, char* argv[])
 {
   return Catch::Session().run(argc, argv);
