71e3267b1ebaf8a164a002bbe7ca96120a6ed890
Silvie Böning <silvie.boening@t-online>
2018-04-22 10:34:50 +0200

    ...

---
+++ source/sumMultiples/sumMultiples.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include "iostream"
+
+
+
+int main()
+{
+    int sum = 0;
+    int i = 1;
+    while(i <= 1000){
+        if(i % 3 == 0 || i % 3 == 0){
+            sum = sum + i;
+        }
+    }
+    std::cout << sum << std::endl;
+}
+      
+
+    
+
+    
+    /*
+    for(int i= 0; i <= 1000; i++)
+        sum+=i;
+    std::cout << "Summe ist " << sum << std::endl;
+}*/
